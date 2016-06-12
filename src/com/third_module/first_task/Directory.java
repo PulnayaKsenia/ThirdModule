adda0411d87eeb32cbeb1488e559875e1c6cc745
PulnayaKsenia <pulnayaksenia@gmail.com>
2016-06-12 17:18:40 +0300

    Initial commit

---
+++ src/com/third_module/first_task/Directory.java
@@ -0,0 +1,15 @@
+package com.third_module.first_task;
+
+import java.util.List;
+
+public class Directory {
+    private List<File> files;
+
+    public List<File> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<File> files) {
+        this.files = files;
+    }
+}
